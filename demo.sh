8cfa07b1e7ebceed9dd8c66c4878183e194cdb68
[m rtk] <gosia.rtk@gmail.com>
2021-06-23 13:58:09 +0200

    user can send message, basic implementation of inbox and sent

---
+++ demo.sh
@@ -0,0 +1,4 @@
+cd ~/Documents/PY_projects/mail
+(python manage.py runserver)
+google-chrome 'https://cs50.harvard.edu/web/2020/projects/3/mail/'
+google-chrome 'https://github.com/gosia1138/CS50w_mail/issues'q
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,14 +20,52 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('form').onsubmit = function() {
+      fetch('/emails', {
+          method: 'POST',
+          body: JSON.stringify({
+              recipients: document.querySelector('#compose-recipients').value,
+              subject: document.querySelector('#compose-subject').value,
+              body: document.querySelector('#compose-body').value,
+          })
+      })
+      .then(response => response.json())
+      .then(result => {
+         console.log(result);
+         if (!result['error']) {
+             load_mailbox('sent');
+         }
+
+      });
+
+      return false;
+  };
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  const emails_view = document.querySelector('#emails-view')
+  emails_view.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Create empty ul element and append to mailbox view
+  const ul = document.createElement('ul');
+  emails_view.append(ul)
+
+  // Fetch emails from appropriate mailbox and append them as li inside ul
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      emails.forEach(function(email) {
+          const li = document.createElement('li')
+          li.innerHTML = `from: ${email.sender}, subject: ${email.subject}, at: ${email.timestamp}`
+          document.querySelector('ul').appendChild(li)
+      })
+
+  })
 }
